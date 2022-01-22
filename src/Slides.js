ea8b9ef59c2d129a45e99024bdfc9c1836f3273e
Aryan Khandelwal <khandelwalaryan12@gmail.com>
2022-01-22 13:39:04 +0530

    first commit

---
+++ src/Slides.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Slides.css";
+
+const Slides = () => {
+  return (
+    <div className="slides">
+      <div className="text">
+        <span>Criminal Justice : Behind Closed Doors</span>
+        <p className="hot-spec">Hotstar Specials · Crime </p>
+        <h6 className="desc">
+          Aditya is a cab driver who has a one-night stand with his passenger
+ 
+        </h6>
+      </div>
+      <div className="images">
+        <img
+          src="https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/1372/871372-h"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Slides;
